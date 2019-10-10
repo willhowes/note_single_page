9145cf7b2c498dfd6822f2cde65f74fbf30c94fb
eldenhowes <williameldenhowes@gmail.com>
2019-10-10 09:37:37 +0100

    Test passes for NotListView handling one note only

--- spec/noteListViewSpec.js
+++ spec/noteListViewSpec.js
@@ -19,5 +19,19 @@ describe('NoteListView', function() {
       expect(this.noteListView.printNotes()).toEqual("<ul><li><div>note one</div></li><li><div>note two</div></li><li><div>note three</div></li></ul>")
     })
   })
+
+  describe('when the fakeNoteList has one note', function(){
+
+    beforeEach(function(){
+      this.fakeNoteList.getNotes.and.callFake(function() {
+        return ['note one'];
+      })
+      this.noteListView = new NoteListView(this.fakeNoteList)
+    });
+  
+    it('returns a string with the notes texts', function() {
+      expect(this.noteListView.printNotes()).toEqual("<ul><li><div>note one</div></li></ul>")
+    })
+  })
 })
 
